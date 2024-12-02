fe366729e18f029989d01250ece1eae84943d49d
Adamnmn <146111813+Adamnmn@users.noreply.github.com>
2024-12-02 14:43:21 -0500

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Day1 {
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+        File text = new File("text");
+        String content = scan.
+        ArrayList<String> list = new ArrayList<String>();
+        for(int i = 0; i<text.length(); i++) {
+
+        }
+
+    }
+}
