24aed671c7a0f72e90deae0da7fa8f4c4630e8c4
Adamnmn <146111813+Adamnmn@users.noreply.github.com>
2024-12-16 14:36:28 -0500

    Update Day3.java

--- Day3.java
+++ Day3.java
@@ -21,12 +21,49 @@ public class Day3 {
         while (m.find()) {
             allMatches.add(m.group());
         }
-        total += getTotal(allMatches);
+        System.out.println(allMatches);
 
-      System.out.println(total);
+        for(String string : allMatches) {
+            String num1="";
+            String num2="";
+            boolean next = false;
+            for(int i = 0; i<string.length(); i++) {
+                if((isNumeric(string.substring(i,i+1)) && !next)) {
+                    num1+=string.substring(i, i+1);
+                }
+                if(string.substring(i,i+1).equals(",")) {
+                    next = true;
+                }
+                if(next){
+                    if(isNumeric(string.substring(i,i+1))) {
+                        num2+=string.substring(i,i+1);
+                    }
+                }
+            }
+            int firstNum = Integer.parseInt(num1);
+            int secondNum = Integer.parseInt(num2);
+            System.out.println(firstNum + " " + secondNum);
+           int third= firstNum*secondNum;
+            total+=third;
+        }
+
+        System.out.println(total);
+//        total += getTotal(allMatches);
+//        System.out.println(total);
 
     }
 
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
 
     public static int getTotal(ArrayList<String> matches) {
         int total = 0;
